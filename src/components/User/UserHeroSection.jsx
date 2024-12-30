61de674447c9381c0f67597ada90c943277e87f0
Praveshkacode <pravesh182003@gmail.com>
2024-12-30 10:48:47 +0530

    adjust border padding

--- src/components/User/UserHeroSection.jsx
+++ src/components/User/UserHeroSection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const UserHeroSection = ({ user }) => (
-  <div className="relative flex justify-center items-center bg-orange-50 py-5">
+  <div className="relative flex justify-center items-center bg-orange-50 py-5 px-2">
     <div className="relative z-10 text-center px-4 border-4 border-orange-700 rounded-lg shadow-lg bg-white max-w-3xl">
       <p className="text-lg md:text-xl mt-2 text-orange-800">
         🌼 जय गंगा मइया की! 🌼 <br />
