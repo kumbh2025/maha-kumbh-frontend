9109b139b193e52b3533cb2cabc4b0dc0383f755
Praveshkacode <pravesh182003@gmail.com>
2025-01-03 18:39:37 +0530

    change in user count content

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Header = ({ userCount }) => (
   <div className="text-center mt-4">
     <h1 className="text-2xl font-bold text-orange-700">
-      Welcome to Maha Kumbh and Happy New Year 2025🥳
+      Welcome to Maha Kumbh 2025🥳
     </h1>
     <p className="text-lg mt-2 text-orange-600">
-      Total Registered Users: <span className="font-bold">{userCount}</span>
+      Currently Registered Users: <span className="font-bold">{userCount}</span>
     </p>
   </div>
 );
