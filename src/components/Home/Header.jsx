b1bf9b2db21077e8bd768da7df5d1682c714e27a
Praveshkacode <pravesh182003@gmail.com>
2025-01-05 23:31:50 +0530

    change in user count content

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -3,10 +3,13 @@ import React from "react";
 const Header = ({ userCount }) => (
   <div className="text-center mt-4">
     <h1 className="text-2xl font-bold text-orange-700">
-      Welcome to Maha Kumbh 2025🥳
+      Welcome to Maha Kumbh 2025
     </h1>
     <p className="text-lg mt-2 text-orange-600">
-      Currently Registered Users: <span className="font-bold">{userCount}</span>
+      Currently Registered Users:{" "}
+      <span className="font-bold">
+        {userCount === 0 ? "Loading..." : userCount}
+      </span>
     </p>
   </div>
 );
